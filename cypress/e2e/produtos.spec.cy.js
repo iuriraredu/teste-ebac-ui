7c572f8b37601f04b8f28d22886cabdd475693cd
Iuri Ramos Reducino <iuri.ramos.reducino@gmail.com>
2023-09-19 22:36:54 -0300

    alterações nos testes automatizados

---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,31 @@
+/// <reference types='cypress' />
+
+describe('Funcionalidade Página de Produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first() // Pegar o primeiro item da lista
+            //.last() // Pegar o ultimo item da lista
+            //.eq(3) // Pegar um item pelo seu indice na lista
+            .contains('Ariel Roll Sleeve Sweatshirt') // Pegar um item pelo texto que contem na lista
+            .click()
+    });
+
+    it.only('Deve adicionar um produto ao carrinho ', () => {
+        var quantidade = 5;
+
+        cy.get('[class="product-block grid"]').contains('Abominable Hoodie').click()
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain',quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+
+    });
+
+});
